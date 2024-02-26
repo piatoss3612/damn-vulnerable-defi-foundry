a157569f53e9134dafdb96c5f10d6f99a75987d1
piatoss <crewe1746@naver.com>
2024-02-26 21:59:50 +0900

    Update 12.Climber v3

--- test/Levels/12.climber/Climber.t.sol
+++ test/Levels/12.climber/Climber.t.sol
@@ -8,15 +8,19 @@ import "forge-std/Test.sol";
 import {DamnValuableToken} from "../../../src/Contracts/DamnValuableToken.sol";
 import {ClimberTimelock} from "../../../src/Contracts/12.climber/ClimberTimelock.sol";
 import {ClimberVault} from "../../../src/Contracts/12.climber/ClimberVault.sol";
+import "../../../src/Contracts/12.climber/ClimberConstants.sol";
+import "../../../src/Contracts/12.climber/ClimberErrors.sol";
 
 contract Climber is Test {
     uint256 internal constant VAULT_TOKEN_BALANCE = 10_000_000e18;
+    uint256 internal constant ATTACKER_INITIAL_ETH_BALANCE = 0.1 ether;
+    uint256 internal constant TIMELOCK_DELAY = 1 hours;
 
     Utilities internal utils;
     DamnValuableToken internal dvt;
-    ClimberTimelock internal climberTimelock;
-    ClimberVault internal climberImplementation;
-    ERC1967Proxy internal climberVaultProxy;
+    ClimberTimelock internal timelock;
+    ClimberVault internal vaultImplementation;
+    ERC1967Proxy internal vaultProxy;
     address[] internal users;
     address payable internal deployer;
     address payable internal proposer;
@@ -36,30 +40,44 @@ contract Climber is Test {
 
         attacker = payable(address(uint160(uint256(keccak256(abi.encodePacked("attacker"))))));
         vm.label(attacker, "Attacker");
-        vm.deal(attacker, 0.1 ether);
+        vm.deal(attacker, ATTACKER_INITIAL_ETH_BALANCE);
+
+        assertEq(attacker.balance, ATTACKER_INITIAL_ETH_BALANCE);
 
         // Deploy the vault behind a proxy using the UUPS pattern,
         // passing the necessary addresses for the `ClimberVault::initialize(address,address,address)` function
-        climberImplementation = new ClimberVault();
-        vm.label(address(climberImplementation), "climber Implementation");
+        vaultImplementation = new ClimberVault();
+        vm.label(address(vaultImplementation), "ClimberVault Implementation");
 
         bytes memory data = abi.encodeWithSignature("initialize(address,address,address)", deployer, proposer, sweeper);
-        climberVaultProxy = new ERC1967Proxy(address(climberImplementation), data);
+        vaultProxy = new ERC1967Proxy(address(vaultImplementation), data);
+        vm.label(address(vaultProxy), "ClimberVault Proxy");
 
-        assertEq(ClimberVault(address(climberVaultProxy)).getSweeper(), sweeper);
+        assertEq(ClimberVault(address(vaultProxy)).getSweeper(), sweeper);
 
-        assertGt(ClimberVault(address(climberVaultProxy)).getLastWithdrawalTimestamp(), 0);
+        assertGt(ClimberVault(address(vaultProxy)).getLastWithdrawalTimestamp(), 0);
 
-        climberTimelock = ClimberTimelock(payable(ClimberVault(address(climberVaultProxy)).owner()));
+        // Instantiate timelock
+        address timelockAddress = ClimberVault(address(vaultProxy)).owner();
+        vm.label(timelockAddress, "ClimberTimelock");
 
-        assertTrue(climberTimelock.hasRole(climberTimelock.PROPOSER_ROLE(), proposer));
+        timelock = ClimberTimelock(payable(timelockAddress));
 
-        assertTrue(climberTimelock.hasRole(climberTimelock.ADMIN_ROLE(), deployer));
+        assertEq(timelock.delay(), TIMELOCK_DELAY);
+
+        vm.expectRevert(CallerNotTimelock.selector);
+        timelock.updateDelay(uint64(TIMELOCK_DELAY) + 1);
+
+        assertTrue(timelock.hasRole(PROPOSER_ROLE, proposer));
+
+        assertTrue(timelock.hasRole(ADMIN_ROLE, deployer));
+
+        assertTrue(timelock.hasRole(ADMIN_ROLE, timelockAddress));
 
         // Deploy token and transfer initial token balance to the vault
         dvt = new DamnValuableToken();
         vm.label(address(dvt), "DVT");
-        dvt.transfer(address(climberVaultProxy), VAULT_TOKEN_BALANCE);
+        dvt.transfer(address(vaultProxy), VAULT_TOKEN_BALANCE);
 
         console.log(unicode"🧨 Let's see if you can break it... 🧨");
     }
@@ -81,6 +99,6 @@ contract Climber is Test {
          * SUCCESS CONDITIONS
          */
         assertEq(dvt.balanceOf(attacker), VAULT_TOKEN_BALANCE);
-        assertEq(dvt.balanceOf(address(climberVaultProxy)), 0);
+        assertEq(dvt.balanceOf(address(vaultProxy)), 0);
     }
 }
--- test/Levels/12.climber/README.md
+++ test/Levels/12.climber/README.md
@@ -1,14 +1,14 @@
 # Challenge #12 - Climber
-There's a secure vault contract guarding 10 million DVT tokens. The vault is upgradeable, following the [UUPS pattern](https://eips.ethereum.org/EIPS/eip-1822).
+
+There’s a secure vault contract guarding 10 million DVT tokens. The vault is upgradeable, following the [UUPS pattern](https://eips.ethereum.org/EIPS/eip-1822).
 
 The owner of the vault, currently a timelock contract, can withdraw a very limited amount of tokens every 15 days.
 
-On the vault there's an additional role with powers to sweep all tokens in case of an emergency.
+On the vault there’s an additional role with powers to sweep all tokens in case of an emergency.
 
-On the timelock, only an account with a "Proposer" role can schedule actions that can be executed 1 hour later.
+On the timelock, only an account with a “Proposer” role can schedule actions that can be executed 1 hour later.
 
-Your goal is to empty the vault.
+To pass this challenge, take all tokens from the vault.
 
-[See the contracts](https://github.com/nicolasgarcia214/damn-vulnerable-defi-foundry/tree/master/src/Contracts/climber)
-<br/>
-[Complete the challenge](https://github.com/nicolasgarcia214/damn-vulnerable-defi-foundry/blob/master/test/Levels/climber/Climber.t.sol)
+- [See the contracts](https://github.com/piatoss3612/damn-vulnerable-defi-foundry/tree/v3/src/Contracts/12.climber)
+- [Complete the challenge](https://github.com/piatoss3612/damn-vulnerable-defi-foundry/blob/v3/test/Levels/12.climber/Climber.t.sol)
